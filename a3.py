6fc6d4d07fd2c59b9b108300a5fc8b80c299d2ae
gracekumagai <g.kumagai@mail.utoronto.ca>
2017-03-20 05:47:21 -0400

    grace's stuff

--- a3.py
+++ a3.py
@@ -571,37 +571,6 @@ class Classifier(object):
 		plt.legend(loc=4)
 		plt.savefig("Part4_logreg_perf.png")
 
-	def part7(self, num_iterations=250, alpha=0.001):
-		train_perf, test_perf = self.LogisticRegression(num_iterations=num_iterations,
-			alpha=alpha, part4=False)
-
-		plt.figure()
-		plt.title("Part 7: Using LogisticRegression for word2vec")
-		plt.ylabel("Performance")
-		plt.xlabel("Iterations")
-		plt.plot(train_perf, label="Training Accuracy")
-		plt.plot(test_perf, label="Testing Accuracy")
-		plt.legend(loc=4)
-		plt.savefig("Part7_logreg_perf.png")
-
-	def part8(self):
-		pos_review_class = self.classes["positive"]
-		neg_review_class = self.classes["negative"]
-		all_reviews = pos_review_class._train_set + neg_review_class._train_set
-
-		words = ['story', 'good', 'oscar', 'plotholes']
-
-		closest_story = all_reviews.isInContext_embeddings_count(words[0])
-		closest_good = all_reviews.isInContext_embeddings_count(words[1])
-		closest_oscar = all_reviews.isInContext_embeddings_count(words[2])
-		closest_plotholes = all_reviews.isInContext_embeddings_count(words[3])
-
-		print ("The 10 closest words with highest frequency to {} are {}".format(words[0], closest_story))
-		print ("The 10 closest words with highest frequency to {} are {}".format(words[1], closest_good))
-		print ("The 10 closest words with highest frequency to {} are {}".format(words[2], closest_oscar))
-		print ("The 10 closest words with highest frequency to {} are {}".format(words[3], closest_plotholes))
-
-
 if __name__ == '__main__':
 
 	classes = {}
@@ -616,5 +585,3 @@ if __name__ == '__main__':
 	#classifier.part3(n=10)
 
 	#classifier.part4()
-	classifier.part7()
-	#classifier.part8()
